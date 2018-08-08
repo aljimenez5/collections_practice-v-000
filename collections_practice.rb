ec7ae2e9096a914b1b0d47f34c3626b6bdf636d3
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 18:25:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ def sum_array(integer_arr)
 end
 
 def add_s(word_arr)
-  word_arr.each_with_index.collect {|word, index| word + "s" unless word[index] == 0 return word}
+  word_arr.each_with_index.collect {|word, index| word + "s" except word[1] }
 end
   
 
